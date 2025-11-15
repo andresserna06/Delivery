42861035b6d4c52bbaec8b0c7999620e6c079472
BrayanAC-18 <acevedobrayan320@gmail.com>
2025-11-15 17:16:24 -0500

    Guardian

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { AuthenticationGuard } from './guards/authentication.guard';
 
-const routes: Routes =[
+const routes: Routes = [
   {
     path: '',
     redirectTo: 'dashboard',
@@ -14,6 +15,7 @@ const routes: Routes =[
   }, {
     path: '',
     component: AdminLayoutComponent,
+    canActivate: [AuthenticationGuard],   // <--- AQUÍ
     children: [
       {
         path: '',
@@ -39,7 +41,7 @@ const routes: Routes =[
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
+    RouterModule.forRoot(routes, {
       useHash: true
     })
   ],
